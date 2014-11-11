c7ef790b262045e1f74e6cc8247007ce4b1db135
AndrewShidel <andrew@shidel.com>
2014-11-11 10:17:51 -0500

    Implemented Tick.java as a static class.

--- src/Tick.java
+++ src/Tick.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
 public class Tick {
-  private ArrayList<TickEvent> events;
-  private int ticks = 0;
+  private static ArrayList<TickEvent> events = new ArrayList<TickEvent>();
+  private static int ticks = 0;
   public Tick(){
-    events = new ArrayList<TickEvent>();
+    //events = new ArrayList<TickEvent>();
   }
-  public void tick(){
+  public static void tick(){
     ++ticks;
     TickEvent event;
     for (int i=0; i<events.size(); i++){
@@ -17,13 +17,13 @@ public class Tick {
       }
     }
   }
-  public void addHandler(TickEvent event){
+  public static void addHandler(TickEvent event){
     event.nextRun = event.frequency;
     events.add(event);
   }
   public abstract class TickEvent {
     public int frequency=0;
-    public int nextRun=0;
+    public int nextRun;
     public abstract void run();
   }
 }
